60795cf9e5a48020476a7a7a575551b2d73d59d7
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2018-03-23 10:55:17 +0300

    missing number

---
+++ array_interview_qtns/missing_number.rb
@@ -0,0 +1,20 @@
+# you are given an array of numbers from 1 to 100 that has one missing number
+# Find the missing number
+
+def missing_number(array)
+  expected_sum = 101 * 50 # (n(n + 1))/2
+  array_sum = sum(array)
+  expected_sum - array_sum
+end
+
+def missing_number2(array) # first 10 numbers
+  expected_sum = 11 * 5
+  array_sum = 0
+  array.each do |num|
+    array_sum + num
+  end
+
+  expected_sum - array_sum
+end
+sample = [1, 2, 3, 4, 5, 6, 8, 9, 10]
+puts(missing_number2(sample))
